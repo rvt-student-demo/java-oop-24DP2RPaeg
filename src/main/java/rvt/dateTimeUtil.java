43d7c69d6e0761e2b3bdd70f00955aa98b5acb39
24DP2RPaeg <A242229rp@rvt.lv>
2026-06-01 14:19:59 +0000

    Studentu reģistrācijas sistēma

---
+++ src/main/java/rvt/dateTimeUtil.java
@@ -0,0 +1,16 @@
+package rvt;
+
+import java.time.LocalDateTime; // importē datumu un laiku
+
+import java.time.format.DateTimeFormatter; // datuma un laika formatejumam
+
+public class dateTimeUtil { // klase, kas atgriež pašreizējo laiku un datumu, kā String
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-dd-MM HH:mm:ss"); // formāts, 
+    // kādā tiks saglabāts datums un laiks
+
+    public static String nowAsString() { // metode, kas atgriež pašreizējo datumu un laiku
+
+        return LocalDateTime.now().format(formatter);
+    }
+}
